db9a4b5823290d4b208909b96879e8f304e78398
MAHDTech <MAHDTech@saltlabs.tech>
2025-10-18 00:12:20 +1100

    fix: git hooks

--- devenv.nix
+++ devenv.nix
@@ -125,15 +125,7 @@ in
   };
 
   git-hooks = {
-    excludes = [
-      ".cache"
-      ".devenv"
-      ".direnv"
-      ".git"
-      ".vscode"
-      "bundle"
-      "vendor"
-    ];
+    excludes = [ ];
     hooks = {
       actionlint.enable = true;
       action-validator.enable = true;
@@ -142,7 +134,7 @@ in
       check-shebang-scripts-are-executable.enable = true;
       check-symlinks.enable = true;
       check-yaml.enable = true;
-      commitizen.enable = false; # TODO: Enable when upstream is fixed.
+      commitizen.enable = true;
       convco.enable = true;
       deadnix.enable = true;
       dialyzer.enable = true;
@@ -177,9 +169,7 @@ in
       pre-commit-hook-ensure-sops.enable = true;
       prettier = {
         enable = true;
-        excludes = [
-          "module/README.md"
-        ];
+        excludes = [ ];
       };
       # Use prettier instead.
       pretty-format-json.enable = false;
